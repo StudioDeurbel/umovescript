79ad6475b736e7ec26f3b242ba028ce6c50c1491
StudioDeurbel <nikenaukjeandries@gmail.com>
2025-02-03 11:23:14 +0100

    Added umove.py

---
+++ umove.py
@@ -0,0 +1,13 @@
+print("Welcome to Project UMove")
+umove = input("Do you want to join Project UMove (Y/N): ")
+if umove == "Y":
+    print("Welcome to Project UMove. I will make your life better.")
+elif umove == "N":
+    print("But won't you let me make your life better. Hey there baby dont you lose my number.")
+else:
+    print("I assume thats a yes. So, ill make your life better.")
+
+#Written by: Nik Andries
+#Written in: PyCharm
+#Inspired by: CG5's Project UMove and UMove
+#@StudioDeurbel 2025
